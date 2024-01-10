033d356ec1636083a376acfdaa3dca4a562f8415
yoseph2202 <77262413+yoseph2202@users.noreply.github.com>
2024-01-09 22:45:05 -0500

    Update economia.php

--- secciones/economia.php
+++ secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Crecimiento estable de la economía impulsado por sectores clave",
+"autor" => "nosotros",
+"resumen" => "El país ha experimentado un crecimiento económico estable en el último trimestre, impulsado por el sólido desempeño de sectores clave como la industria manufacturera, la tecnología y el turismo. Los indicadores económicos muestran un aumento constante en la producción industrial, reflejando una recuperación sólida tras periodos de desafíos económicos.",
 ];
 ?>
