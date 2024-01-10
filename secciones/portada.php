db041690d7180edcbbdefddb48dea6586d7db526
yoseph2202 <77262413+yoseph2202@users.noreply.github.com>
2024-01-09 21:39:19 -0500

    Add files via upload

---
+++ secciones/portada.php
@@ -0,0 +1,11 @@
+<?php
+/*****
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+*****/
+
+$portada = [
+"titulo" => "REVISTA UNS 2024",
+"autor" => "uns",
+"resumen" => "Universidad Nacional del Santa",
+];
+?>
