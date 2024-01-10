7668c719c6a0af60177c278048edf9f93d0f78ef
yoseph2202 <77262413+yoseph2202@users.noreply.github.com>
2024-01-09 22:34:57 -0500

    Update internacional.php

--- secciones/internacional.php
+++ secciones/internacional.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La Nasa",
+"autor" => "Yo",
+"resumen" => "
+Hasta mi última actualización en enero de 2022, no tengo acceso a información en tiempo real, incluyendo noticias actuales. Te recomendaría consultar fuentes de noticias confiables para obtener la información más reciente. Sin embargo, puedo ofrecerte un ejemplo genérico de cómo podría ser una noticia internacional pequeña:",
 ];
 ?>
