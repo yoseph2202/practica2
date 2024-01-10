f6533b37ffd958b88c67c16d71551a4cac756d83
yoseph2202 <77262413+yoseph2202@users.noreply.github.com>
2024-01-09 22:40:19 -0500

    Update nacional.php

--- secciones/nacional.php
+++ secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Inauguración de proyecto de infraestructura para mejorar la conectividad en regiones remotas",
+"autor" => "Tú",
+"resumen" => "Hoy, se llevó a cabo la inauguración de un proyecto de infraestructura vial en regiones remotas del país. Este proyecto tiene como objetivo mejorar la conectividad y acceso a servicios básicos para comunidades aisladas en zonas rurales. La nueva carretera, construida con tecnología moderna y sostenible, conectará varias localidades que antes enfrentaban dificultades de acceso. Autoridades locales y líderes comunitarios celebraron esta iniciativa como un paso crucial para el desarrollo socioeconómico de la región, facilitando el transporte de bienes y personas, y promoviendo oportunidades de crecimiento para las poblaciones locales.",
 ];
 ?>
