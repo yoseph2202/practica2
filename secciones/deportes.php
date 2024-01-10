235bb76c11af7f7254c8bf2a47234d2bfb027467
yoseph2202 <77262413+yoseph2202@users.noreply.github.com>
2024-01-09 22:41:16 -0500

    Update deportes.php

--- secciones/deportes.php
+++ secciones/deportes.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $deportes = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Equipo nacional logra un hito en un campeonato internacional",
+"autor" => "él",
+"resumen" => "El equipo nacional de fútbol/baloncesto/tenis (u otro deporte) alcanzó un logro histórico en el campeonato internacional disputado en [ubicación]. Después de una actuación excepcional y emocionantes enfrentamientos en la etapa final del torneo, el equipo logró llegar a la final/semifinal/etapa crucial, destacándose por su rendimiento excepcional y su espíritu competitivo.",
 ];
 ?>
