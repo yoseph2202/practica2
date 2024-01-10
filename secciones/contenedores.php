48fb6122adc2f1a98bfa21285098c1479673f28c
yoseph2202 <77262413+yoseph2202@users.noreply.github.com>
2024-01-09 22:16:19 -0500

    Update contenedores.php

--- secciones/contenedores.php
+++ secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "pruebaaaa",
+"autor" => "pruebaaa",
+"resumen" => "pruebaaa",
 ];
 ?>
